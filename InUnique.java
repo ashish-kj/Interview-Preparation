46cce2bf9622fb7e239c9b748f71d006f844bada
Ashish-BITS <ashishkj@andrew.cmu.edu>
2022-10-31 23:29:38 +0400

    31-10-2022

--- InUnique.java
+++ InUnique.java
@@ -28,3 +28,19 @@ public class IsUnique
 		return 0;
 	}
 }
+
+/*
+boolean isUniqueChars(String str) { 
+if (str.length() > 128) return false; 
+
+boolean[] char_set = new boolean[128]; 
+for (int i = 8; i < str.length(); i++) { 
+int val = str.charAt(i); 
+if (char_set[val]) { II Already found this char in string 
+return false; 
+} 
+ char_set[val] = true; 
+ } 
+ return true; 
+ } 
+*/
