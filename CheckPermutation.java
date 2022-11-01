a8169321257f589d08a1fa694238d7d20953e06c
Ashish-BITS <ashishkj@andrew.cmu.edu>
2022-11-01 17:05:21 +0400

    Update CheckPermutation.java
    01-11-2022

--- CheckPermutation.java
+++ CheckPermutation.java
@@ -2,6 +2,7 @@
 Check Permutation: Given two strings, write a method to decide if one is a permutation of the 
 other. 
 */
+//My Solution :1
 import java.util.*;
 
 public class CheckPermutation
@@ -24,7 +25,79 @@ public class CheckPermutation
 	        return false;
 	}
 }
+
+//My Solution :2
+/*
+import java.util.*;
+public class CheckPermutation
+{
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		CheckPermutation obj = new CheckPermutation();
+		String s1, s2;
+		s1 = s.nextLine();
+		s2 = s.nextLine();
+		if(s1.length()==s2.length())
+		    System.out.println(obj.checkPermutation(s1,s2));
+		else
+		    System.out.println("Not a permutation");
+		
+	}
+	boolean checkPermutation(String s1, String s2){
+	    HashMap<Character,Integer> h = new HashMap<Character,Integer>();
+	    int i;
+	    for(i=0;i<s1.length();i++){
+	        if(h.get(s1.charAt(i))==null){
+	            h.put(s1.charAt(i),1);
+	        }
+	        else    
+	            h.put(s1.charAt(i),h.get(s1.charAt(i))+1);
+	    }
+	    for(i=0;i<s1.length();i++){
+	        if(h.get(s2.charAt(i))==null){
+	            return false;
+	        }
+	        else    
+	        {
+	            if(h.get(s2.charAt(i))>0)
+	                h.put(s2.charAt(i),h.get(s2.charAt(i))-1);
+	            else
+	                return false;
+	        }
+	    }
+	    return true;
+	}
+	
+}
+*/
+
 /*
 runtime: o(nlong), n = char array length
 space = o(2n)= o(n)
+*/
+
+/*
+Solution:
+boolean permutation(String s, String t) { 
+if (s.lengthO != t.lengthO) { 
+return false ; 
+} 
+
+int[] letters = new int[128]; II Assumption 
+
+char[] s_array = s.toCharArray(); 
+for (char c : s_array) { II count number of each char i n s. 
+ letters[c]++; 
+ } 
+ 
+ for (int i = e; i < t.length(); i++) { 
+ int c = (int) t . charAt(i)j 
+ letters[c] --; 
+ if (letters[c] < 0) { 
+ return false; 
+ } 
+ } 
+ 
+ return true; 
+ } 
 */
