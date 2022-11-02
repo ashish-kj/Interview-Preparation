c1294ad85669ecf1aec538782b694ecf7856fcd4
Ashish-BITS <ashishkj@andrew.cmu.edu>
2022-11-02 18:25:10 +0400

    Update WholeMinuteDilemma.java

--- WholeMinuteDilemma.java
+++ WholeMinuteDilemma.java
@@ -25,3 +25,26 @@ public class WholeMinuteDilemma
         return ans;
     }
 }
+/*
+Solution:
+Class Solution{
+    public int numPairsDivisibleBy60(int[] time){
+        int[] count = new int[60];
+        for(int i:time){ count[i%60]++; }
+
+        int pair = 0;
+
+        for(int i=1;i<30;i++){
+            pair+= count[i]*count[60-i];
+        }
+
+        pair+=sumofN(count[0]-1)+sumofN(count[30]-1);
+
+        return pair;
+    }
+
+    int sumofN(int n){
+        return n*(n+1)/2;
+    }
+}
+*/
