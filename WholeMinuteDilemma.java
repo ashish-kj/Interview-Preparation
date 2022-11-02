a76439a082ecd079a3763152cc20a56098c69a89
Ashish-BITS <ashishkj@andrew.cmu.edu>
2022-11-02 18:10:20 +0400

    Create WholeMinuteDilemma.java

---
+++ WholeMinuteDilemma.java
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/pairs-of-songs-with-total-durations-divisible-by-60
+import java.util.*;
+public class WholeMinuteDilemma
+{
+	public static void main(String[] args) {
+		int time[] = {10, 20, 30, 60, 80, 110, 120};
+		WholeMinuteDilemma obj = new WholeMinuteDilemma();
+		int res = obj.numPairsDivisibleBy60(time);
+		System.out.println(res);
+	}
+	int numPairsDivisibleBy60(int[] time) {
+        int n = time.length, ans = 0;
+        int rem, target,i;
+        HashMap<Integer, Integer> len = new HashMap<Integer,Integer>();
+        for(i  = 0; i < n; i++){
+            rem = time[i] % 60;
+            target = 60 - (rem);
+            if(len.containsKey(target))
+                ans += len.get(target);
+            if(rem != 0)
+                len.put(rem, len.getOrDefault(rem, 0) + 1);
+            else
+                len.put(60, len.getOrDefault(60, 0) + 1);
+        }
+        return ans;
+    }
+}
